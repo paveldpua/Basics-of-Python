26d5837671f5ba67570e5c13455f8d9abdf5b7a8
Pavlo Petrychenko <Pavlo_Petrychenko@epam.com>
2020-06-08 14:51:13 +0200

    introduced constants, added defaultdict and moved check for dict type to earliest stage

--- tasks/Module 2/task_1.py
+++ tasks/Module 2/task_1.py
@@ -1,14 +1,18 @@
 """Work with inventory."""
+import collections
+MAX_INVENTORY_CAP = 80
+HEAVY_WEIGHT_THRESHOLD = 70
+LIGHT_WEIGHT_THRESHOLD = 60
 
 
 def display_inventory(inventory):
     """Print inventory."""
-    print('Inventory:')
-    items_count = 0
     # check that inventory is a dict
     if not isinstance(inventory, dict):
         print('Wrong inventory type. Should be dict')
         return
+    print('Inventory:')
+    items_count = 0
     # print inventory and calculate sum of items
     for key, value in inventory.items():
         print(value, key)
@@ -16,26 +20,22 @@ def display_inventory(inventory):
     print()
     # check number of item and print according caution if needed
     print('Total number of items:', items_count, end='\n\n')
-    if 60 <= items_count <= 69:
+    if LIGHT_WEIGHT_THRESHOLD <= items_count < HEAVY_WEIGHT_THRESHOLD:
         print('CAUTION: Your backpack weighs a lot, your stamina runs out quicker!')
-    elif 70 <= items_count <= 79:
+    elif HEAVY_WEIGHT_THRESHOLD <= items_count < MAX_INVENTORY_CAP:
         print('CAUTION: Your equipment is very heavy, you\'re moving slower than usual!')
-    elif items_count >= 80:
+    elif items_count >= MAX_INVENTORY_CAP:
         print('CAUTION: You are overloaded, can\'t move!')
 
 
 def add_to_inventory(inventory, added_items):
     """Add items from list to inventory."""
-    skipped = {}
+    skipped = collections.defaultdict(int)
     added_items_number = 0
     rubbish = ['rubbish', 'chewed gum', 'used tissue']
-
     for item in added_items:
         if item in rubbish:
-            if item in skipped:
-                skipped[item] += 1
-            else:
-                skipped[item] = 1
+            skipped[item] += 1
         else:
             if item in inventory:
                 inventory[item] += 1
@@ -54,5 +54,5 @@ hero_inventory = {'gold coin': 42, 'rope': 1}
 dragon_loot = [
     'gold coin', 'chewed gum', 'dagger', 'gold coin', 'gold coin', 'ruby',
     'rubbish', 'chewed gum', 'used tissue']
-inv = add_to_inventory(hero_inventory, dragon_loot)
+hero_inventory = add_to_inventory(hero_inventory, dragon_loot)
 display_inventory(hero_inventory)
