4cc83c9f8f5a6b92f8d75199d4874a48d70bc5ed
Pavlo Petrychenko <Pavlo_Petrychenko@epam.com>
2020-07-02 21:24:39 +0200

    task 5

---
+++ tasks/Module 4/task_5.py
@@ -0,0 +1,18 @@
+"""Find Mistakes in a Spreadsheet."""
+import ezsheets
+
+
+def find_mistakes(spreadsheet_id):
+    """Find Mistakes in a Spreadsheet."""
+    spreadsheet = ezsheets.Spreadsheet(spreadsheet_id)
+    row = 2
+    while True:
+        if int(spreadsheet[0].getRow(row)[0]) * int(spreadsheet[0].getRow(row)[1]) \
+                != int(spreadsheet[0].getRow(row)[2]):
+            print(f'Row {row} has mistake')
+            break
+        row += 1
+
+
+if __name__ == '__main__':
+    find_mistakes('1jDZEdvSIh4TmZxccyy0ZXrH-ELlrwq8_YYiZrEOB4jg')
