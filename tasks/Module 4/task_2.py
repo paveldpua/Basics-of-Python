fbb55fade5326c059d5805aa4ebebb144822ecc9
Pavlo Petrychenko <Pavlo_Petrychenko@epam.com>
2020-07-03 14:46:22 +0200

    adjustments from first round of review, attempt 2

--- tasks/Module 4/task_2.py
+++ tasks/Module 4/task_2.py
@@ -5,18 +5,14 @@ MIN_ZEROES = 3
 
 def fill_gaps(folder_path, prefix):
     """Fill in the Gaps."""
-    # init file list with first not needed element
-    file_list = [None]
     os.chdir(folder_path)
-    # get list of tuples of filenames and extentions
+    # get sorted list of tuples of filenames and extentions
     with os.scandir(folder_path) as scandir:
-        file_list += sorted([os.path.splitext(entry.name) for entry in scandir
+        file_list = sorted([os.path.splitext(entry.name) for entry in scandir
                              if entry.name.startswith(prefix) and entry.is_file()])
     # check if sequence number equal to number in filename and rename it if is not.
     number_of_zeroes = max(MIN_ZEROES, len(str(len(file_list))))
-    for i, file_data in enumerate(file_list):
-        if not file_data:
-            continue
+    for i, file_data in enumerate(file_list, start=1):
         file_name, file_extension = file_data
         number = int(file_name[len(prefix):])
         if i != number:
